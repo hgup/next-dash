fc9cb7ab837d3568ec9357d1ed5e650daaaf2816
Hursh Gupta <hurshgupta1@gmail.com>
2024-01-04 21:21:28 +0530

    validate and prepare data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,24 @@
 'use server';
 
-export async function createInvoice(formData: FormData) {
-  //   const rawFormData = {
-  //     customerId: formData.get('customerId'),
-  //     amount: formData.get('amount'),
-  //     status: formData.get('status'),
-  //   };
-  const rawFormData = Object.fromEntries(formData.entries());
+import { z } from 'zod';
 
-  // Test it out:
-  console.log(rawFormData);
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  //The amount field is specifically set to coerce (change) from a string to a number while also validating its type.
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
 }
