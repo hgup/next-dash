7580d261491ba290497d6d4b3619e39e7d8fde44
Hursh Gupta <hurshgupta1@gmail.com>
2024-01-12 21:49:50 +0530

    authentication implemented

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,12 +17,9 @@ export async function fetchRevenue() {
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
-    console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
